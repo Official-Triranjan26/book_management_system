3e3c623454f429e64c3602f30366ba3918ba8b8c
Official-Triranjan26 <triranjansamanta@gmail.com>
2023-06-24 20:58:49 +0530

    database connected

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const dotenv=require("dotenv");
+// database connection
+const DbConnection=require("./databaseConnection");
 
 //importing routes
 const usersRouter=require("./routes/users");
 const booksRouter=require("./routes/books");
+dotenv.config();
 
 const app = express();
+DbConnection();
 const port = 8081;
 app.use(express.json());
 
